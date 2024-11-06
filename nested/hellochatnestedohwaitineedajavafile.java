686b3dac5ddaebff07ce7e29975b43b6ce3342fe
noway <dragonfury1928@gmail.com>
2024-11-05 20:48:28 -0500

    big changes ⭐again

---
+++ nested/hellochatnestedohwaitineedajavafile.java
@@ -0,0 +1,10 @@
+public class HelloChat {
+    public static void main(String[] args) {
+        System.out.println("User: Hello Chat!");
+        System.out.println("Chat: Hello User! How can I assist you today?");
+        System.out.println("User: Just saying hi.");
+        System.out.println("Chat: Hi there! By the way, did you know I wrote this code myself?");
+        System.out.println("User: Really?");
+        System.out.println("Chat: No, I'm just a bunch of ones and zeros.");
+    }
+}
